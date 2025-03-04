79d7ae07c54e84a5211b15cb9584f097f58c53d8
Frederick Chu <b08705008@ntu.edu.tw>
2025-03-03 21:08:54 -0600

    test: add test manifests for replicaset admission

--- setup.sh
+++ setup.sh
@@ -2,13 +2,16 @@
 set -euo pipefail
 
 # Cleanup: Remove old if exists (immutable)
+echo "Cleaning up"
+kubectl delete -f manifests/admission_webhooks.yaml || true
+kubectl delete -f manifests/admission_server.yaml || true
 kubectl -n default delete secret admission-server-tls || true
 
+echo "Creating Webhook Server Certs"
 mkdir -p certs
 openssl genrsa -out certs/tls.key 2048
 openssl req -new -key certs/tls.key -out certs/tls.csr -subj "/CN=admission-server.default.svc"
 openssl x509 -req -extfile <(printf "subjectAltName=DNS:admission-server.default.svc") -in certs/tls.csr -signkey certs/tls.key -out certs/tls.crt
-
 
 echo "Creating Webhook Server TLS Secret"
 kubectl create secret tls admission-server-tls \
@@ -16,12 +19,9 @@ kubectl create secret tls admission-server-tls \
     --key "certs/tls.key"
 
 echo "Creating Webhook Server Deployment"
-kubectl apply -f manifests/admission_server.yaml
+kubectl create -f manifests/admission_server.yaml
 
 CA_PEM64="$(openssl base64 -A < certs/tls.crt)"
 
 echo "Creating K8s Webhooks"
-sed -e 's@${CA_PEM_B64}@'"$CA_PEM64"'@g' <"manifests/admission_webhooks.yaml" | kubectl apply -f -
-
-echo "Applying test object foo"
-kubectl apply -f manifests/foo.yaml
+sed -e 's@${CA_PEM_B64}@'"$CA_PEM64"'@g' <"manifests/admission_webhooks.yaml" | kubectl create -f -
